0892a2f74c33e9b6e01e3c773cfcda56003ca760
Sean Handley <sean.handley@gmail.com>
2012-11-23 13:12:01 +0000

    Initial Commit.

---
+++ BroadbandDowntime/BroadbandDowntime.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+
+namespace BroadbandDowntime
+{
+    public partial class BroadbandDowntime : ServiceBase
+    {
+        public BroadbandDowntime()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            ThreadStart ts = new ThreadStart(delegate { Main.loop(); });
+            new Thread(ts).Start();
+        }
+
+        protected override void OnStop()
+        {
+
+        }
+    }
+}
---
+++ BroadbandDowntime/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace BroadbandDowntime
+{
+    class Main
+    {
+        public static void loop()
+        {
+            while (true)
+            {
+                CheckIfOnline();
+                Thread.Sleep(1000 * 60);
+            }
+        }
+
+        private static void CheckIfOnline()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
